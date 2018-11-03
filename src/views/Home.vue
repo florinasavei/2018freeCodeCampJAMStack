2a23a37682ed4f6948584b9f66e12c592cf0aa79
Delia <cuibardelia@yahoo.com>
2018-11-03 19:31:17 +0200

    Add sidebar

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,12 @@
 <template>
-  <div>
-    <FavoriteMoviesList />
-    <HelloWorld />
-  </div>
+  <v-layout row wrap>
+    <v-flex xs8>
+      <HelloWorld/>
+    </v-flex>
+    <v-flex xs4>
+      <FavoriteMoviesList/>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
