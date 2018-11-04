2e82a5b62a1234c6f27b603553cd05268a246981
margaritmariancatalin <margaritcatalin97@gmail.com>
2018-11-04 14:22:05 +0200

    Add facebook login

--- src/App.vue
+++ src/App.vue
@@ -6,13 +6,14 @@
         <span class="font-weight-light">MOVIE APP</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
+    <div>
+    <facebook-login class="btn"
+      appId="570316863426221"
+      @login="onLogin"
+      @logout="onLogout"
+      @sdk-loaded="sdkLoaded">
+    </facebook-login>
+    </div>
     </v-toolbar>
 
     <v-content>
@@ -23,16 +24,77 @@
 
 <script>
 import HomeView from "./views/Home";
-
+import facebookLogin from 'facebook-login-vuejs';
 export default {
   name: "App",
   components: {
-    HomeView
+    HomeView,
+    facebookLogin
   },
   data() {
     return {
-      //
-    };
+      isConnected: false,
+      name: '',
+      email: '',
+      personalID: '',
+      FB: undefined
+    }
+  },
+  methods: {
+    getUserData() {
+      this.FB.api('/me', 'GET', { fields: 'id,name,email' },
+        userInformation => {
+          this.personalID = userInformation.id;
+          this.email = userInformation.email;
+          this.name = userInformation.name;
+          console.log(userInformation);
+        }
+      )
+    },
+    sdkLoaded(payload) {
+      this.isConnected = payload.isConnected
+      this.FB = payload.FB
+      if (this.isConnected) this.getUserData()
+    },
+    onLogin() {
+      this.isConnected = true
+      this.getUserData()
+    },
+    onLogout() {
+      this.isConnected = false;
+    }
   }
 };
 </script>
+
+<style>
+.information {
+  margin-top: 100px;
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+}
+.well {
+  background-color: rgb(191, 238, 229);
+  margin: auto;
+  padding: 50px 50px;
+  ;
+  border-radius: 20px;
+  /* display:inline-block; */
+}
+.login {
+  width: 200px;
+  margin: auto;
+}
+.list-item:first-child {
+  margin: 0;
+}
+.list-item {
+  display: flex;
+  align-items: center;
+  margin-top: 20px;
+}
+.button {
+  margin: auto;
+}
+</style>
